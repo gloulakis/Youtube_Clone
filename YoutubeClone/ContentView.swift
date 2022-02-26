6f6c7ce66314546d98107ecdcf0d3fbf53a7504a
George <51861795+gloulakis@users.noreply.github.com>
2022-02-26 10:09:14 +0200

    Initial Commit

---
+++ YoutubeClone/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  YoutubeClone
+//
+//  Created by Georgios Loulakis on 26.02.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ YoutubeClone/YoutubeCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  YoutubeCloneApp.swift
+//  YoutubeClone
+//
+//  Created by Georgios Loulakis on 26.02.22.
+//
+
+import SwiftUI
+
+@main
+struct YoutubeCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
